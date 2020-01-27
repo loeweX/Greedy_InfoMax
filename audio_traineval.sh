c9547ebd3d4b34fe4e431d0c3981dd96125a05d4
Sindy Löwe <loeweX@users.noreply.github.com>
2020-01-27 13:35:13 +0100

    Update audio_traineval.sh

--- audio_traineval.sh
+++ audio_traineval.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "Training the Greedy InfoMax Model on audio data (librispeech)"
-python -m GreedyInfoMax.audio.main_audio --subsample --num_epochs 300 --learning_rate 2e-4 --start_epoch 0 -i ./datasets/ -o . --save_dir audio_experiment
+python -m GreedyInfoMax.audio.main_audio --subsample --num_epochs 1000 --learning_rate 2e-4 --start_epoch 0 -i ./datasets/ -o . --save_dir audio_experiment
 
 echo "Testing the Greedy InfoMax Model for phone classification"
-python -m GreedyInfoMax.audio.linear_classifiers.logistic_regression_phones --model_path ./logs/audio_experiment --model_num 299 -i ./datasets/ -o .
+python -m GreedyInfoMax.audio.linear_classifiers.logistic_regression_phones --model_path ./logs/audio_experiment --model_num 999 -i ./datasets/ -o .
 
 echo "Testing the Greedy InfoMax Model for speaker classification"
-python -m GreedyInfoMax.audio.linear_classifiers.logistic_regression_speaker --model_path ./logs/audio_experiment --model_num 299 -i ./datasets/ -o .
+python -m GreedyInfoMax.audio.linear_classifiers.logistic_regression_speaker --model_path ./logs/audio_experiment --model_num 999 -i ./datasets/ -o .
