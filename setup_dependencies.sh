863b01f59f8b7fabe3b298e6a7fc1da3c2a7d5b8
Sindy Löwe <loeweX@users.noreply.github.com>
2020-06-22 16:45:52 +0200

    Update setup_dependencies.sh

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "Make sure conda is installed."
 echo "Installing environment:"
-conda env create -f environment.yml || exit
+conda env create -f environment.yml
 conda activate infomax
