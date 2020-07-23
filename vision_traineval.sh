d5a050b0cdb5502a6a25b819db0f8de3ac5fc55a
Sindy Löwe <loeweX@users.noreply.github.com>
2020-07-23 17:52:01 +0200

    Update vision_traineval.sh

--- vision_traineval.sh
+++ vision_traineval.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Training the Greedy InfoMax Model on vision data (stl-10)"
-python -m GreedyInfoMax.vision.main_vision --download_dataset --save_dir vision_experiment
+python -m GreedyInfoMax.vision.main_vision --grayscale --download_dataset --save_dir vision_experiment
 
 echo "Testing the Greedy InfoMax Model for image classification"
-python -m GreedyInfoMax.vision.downstream_classification --model_path ./logs/vision_experiment --model_num 299
+python -m GreedyInfoMax.vision.downstream_classification --grayscale --model_path ./logs/vision_experiment --model_num 299
