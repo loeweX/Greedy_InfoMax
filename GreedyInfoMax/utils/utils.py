2e782269356f1e6a3ecf03ef370c4197e64626e9
Richard Baxter <richard.baxter@baxterai.com>
2020-11-29 00:31:12 +1100

    patch utils.py for PyTorch v1.7 compatibility - add correct = correct.contiguous() to def accuracy()

--- GreedyInfoMax/utils/utils.py
+++ GreedyInfoMax/utils/utils.py
@@ -25,6 +25,8 @@ def accuracy(output, target, topk=(1,)):
         pred = pred.t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))
 
+        correct = correct.contiguous()    #required for pytorch V1.7 view()
+
         res = []
         for k in topk:
             correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
